bea710b4ccba2662d4dad59e2c2a3abe9e0d2667
Theanko <55360422+Theanko1412@users.noreply.github.com>
2022-05-23 22:45:52 +0200

    Multiple arguments allowed

--- GlobalLogicJavaAcademy/src/main/java/Main.java
+++ GlobalLogicJavaAcademy/src/main/java/Main.java
@@ -4,15 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        char[] letters = args[0].toLowerCase().toCharArray();
         //using TreeMap so out values are sorted by key
         TreeMap<Character, Integer> letterCount = new TreeMap<>();
 
-        for (char character : letters) {
-            if(!letterCount.containsKey(character)) {
-                letterCount.put(character, 1);
-            } else {
-                letterCount.put(character, letterCount.get(character)+1);
+        for (int i = 0; i < args.length; i++) {
+            char[] letters = args[i].toLowerCase().toCharArray();
+
+            for (char character : letters) {
+                if(!letterCount.containsKey(character)) {
+                    letterCount.put(character, 1);
+                } else {
+                    letterCount.put(character, letterCount.get(character)+1);
+                }
             }
         }
 
